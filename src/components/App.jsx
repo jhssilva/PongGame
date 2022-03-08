1cd52279aaddf2ff9215e28ec397f9667b72f897
Hugo Silva <jhssilva96@gmail.com>
2022-03-08 15:05:39 +0000

    Ball Ready
    Ball ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,34 +2,77 @@ import { createFactory } from "react";
 import "./App.css";
 import Canvas from "./Canvas";
 
+//Axios to communicate with the back-end
+
 function App() {
   const dimensions = {
     width: 650,
     height: 480,
+    bar: {
+      x: 10,
+      y: 70,
+    },
+    ball: {
+      radius: 9,
+    },
   };
 
   const colors = {
-    text: "#000000",
-    players: "#000000",
-    body: "#393E46",
+    text: "#222831",
+    players: "#00ADB5",
+    body: "#EEEEEE",
+    pageBackground: "#393E46",
   };
 
   const draw = (ctx, frameCount, fps) => {
     //Clear screen
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-    //Rectangule
+    // Canvas Body
     ctx.fillStyle = colors.body;
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    // Division Middle
+    ctx.moveTo(ctx.canvas.width / 2, 0);
+    ctx.lineTo(ctx.canvas.width / 2, ctx.canvas.height);
+    ctx.stroke();
 
     // Draw Fps
     ctx.fillStyle = colors.text;
     ctx.font = "20px Arial";
-    ctx.fillText(fps, ctx.canvas.width / 2, ctx.canvas.height - 10);
+    ctx.strokeText(fps, ctx.canvas.width / 2 + 10, ctx.canvas.height - 10);
 
+    // Player 1 Set
+    ctx.fillStyle = colors.players;
+    ctx.fillRect(
+      20,
+      ctx.canvas.height / 2 - dimensions.bar.y / 2,
+      dimensions.bar.x,
+      dimensions.bar.y
+    );
+
+    // Player 2 Set
+    ctx.fillRect(
+      ctx.canvas.width - 20 - dimensions.bar.x / 2,
+      ctx.canvas.height / 2 - dimensions.bar.y / 2,
+      dimensions.bar.x,
+      dimensions.bar.y
+    );
+
+    // Ball
     ctx.fillStyle = colors.players;
     ctx.beginPath();
-    ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
+    ctx.arc(
+      frameCount * 2,
+      frameCount * 2,
+      dimensions.ball.radius,
+      0,
+      2 * Math.PI
+    );
+    ctx.stroke();
+    //ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
+
+    // Paint
     ctx.fill();
   };
 
