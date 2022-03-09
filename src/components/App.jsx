2ffdafefe79e0f9c74a1a8f66879559e665bd372
Hugo Silva <jhssilva96@gmail.com>
2022-03-09 16:24:55 +0000

    Websocket working
    Websocket working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,84 +1,42 @@
-import { createFactory } from "react";
+import React from "react";
 import "./App.css";
-import Canvas from "./Canvas";
+import Game from "./Game";
+import { WebSocketDemo } from "./WebSocketDemo";
 
 //Axios to communicate with the back-end
 
 function App() {
-  const dimensions = {
-    width: 650,
-    height: 480,
-    bar: {
-      x: 10,
-      y: 70,
-    },
-    ball: {
-      radius: 9,
-    },
+  const connectToServer = () => {
+    const endpoint = "wss:localhost:8080";
+    const ws = new WebSocket(endpoint);
+    const apiCall = {
+      event: "play",
+      data: { channel: "order_book_btcusd" },
+    };
+
+    ws.onopen = (event) => {
+      ws.send(JSON.stringify(apiCall));
+    };
+
+    ws.onmessage = function (event) {
+      const json = JSON.parse(event.data);
+      console.log(`[message] Data received from server: ${json}`);
+      try {
+        if ((json.event = "data")) {
+          console.log(json.data);
+        }
+      } catch (err) {
+        // whatever you wish to do with the err
+      }
+    };
   };
 
-  const colors = {
-    text: "#222831",
-    players: "#00ADB5",
-    body: "#EEEEEE",
-    pageBackground: "#393E46",
-  };
-
-  const draw = (ctx, frameCount, fps) => {
-    //Clear screen
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-    // Canvas Body
-    ctx.fillStyle = colors.body;
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-    // Division Middle
-    ctx.moveTo(ctx.canvas.width / 2, 0);
-    ctx.lineTo(ctx.canvas.width / 2, ctx.canvas.height);
-    ctx.stroke();
-
-    // Draw Fps
-    ctx.fillStyle = colors.text;
-    ctx.font = "20px Arial";
-    ctx.strokeText(fps, ctx.canvas.width / 2 + 10, ctx.canvas.height - 10);
-
-    // Player 1 Set
-    ctx.fillStyle = colors.players;
-    ctx.fillRect(
-      20,
-      ctx.canvas.height / 2 - dimensions.bar.y / 2,
-      dimensions.bar.x,
-      dimensions.bar.y
-    );
-
-    // Player 2 Set
-    ctx.fillRect(
-      ctx.canvas.width - 20 - dimensions.bar.x / 2,
-      ctx.canvas.height / 2 - dimensions.bar.y / 2,
-      dimensions.bar.x,
-      dimensions.bar.y
-    );
-
-    // Ball
-    ctx.fillStyle = colors.players;
-    ctx.beginPath();
-    ctx.arc(
-      frameCount * 2,
-      frameCount * 2,
-      dimensions.ball.radius,
-      0,
-      2 * Math.PI
-    );
-    ctx.stroke();
-    //ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
-
-    // Paint
-    ctx.fill();
-  };
+  //connectToServer();
 
   return (
     <div className="App">
-      <Canvas draw={draw} dimensions={dimensions} />
+      {/* <Game /> */}
+      <WebSocketDemo />
     </div>
   );
 }
---
+++ src/components/Game.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import Canvas from "./Canvas";
+
+const Game = (props) => {
+  const dimensions = {
+    width: 650,
+    height: 480,
+    bar: {
+      x: 10,
+      y: 70,
+    },
+    ball: {
+      radius: 9,
+    },
+  };
+
+  const colors = {
+    text: "#222831",
+    players: "#00ADB5",
+    body: "#EEEEEE",
+    pageBackground: "#393E46",
+  };
+
+  const draw = (ctx, frameCount, fps) => {
+    //Clear screen
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    // Canvas Body
+    ctx.fillStyle = colors.body;
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    // Division Middle
+    ctx.moveTo(ctx.canvas.width / 2, 0);
+    ctx.lineTo(ctx.canvas.width / 2, ctx.canvas.height);
+    ctx.stroke();
+
+    // Draw Fps
+    ctx.fillStyle = colors.text;
+    ctx.font = "20px Arial";
+    ctx.strokeText(fps, ctx.canvas.width / 2 + 10, ctx.canvas.height - 10);
+
+    // Player 1 Set
+    ctx.fillStyle = colors.players;
+    ctx.fillRect(
+      20,
+      ctx.canvas.height / 2 - dimensions.bar.y / 2,
+      dimensions.bar.x,
+      dimensions.bar.y
+    );
+
+    // Player 2 Set
+    ctx.fillRect(
+      ctx.canvas.width - 20 - dimensions.bar.x / 2,
+      ctx.canvas.height / 2 - dimensions.bar.y / 2,
+      dimensions.bar.x,
+      dimensions.bar.y
+    );
+
+    // Ball
+    ctx.fillStyle = colors.players;
+    ctx.beginPath();
+    ctx.arc(
+      frameCount * 2,
+      frameCount * 2,
+      dimensions.ball.radius,
+      0,
+      2 * Math.PI
+    );
+    ctx.stroke();
+    //ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
+
+    // Paint
+    ctx.fill();
+  };
+
+  return (
+    <div className="game-container">
+      <Canvas draw={draw} dimensions={dimensions} />
+    </div>
+  );
+};
+
+export default Game;
